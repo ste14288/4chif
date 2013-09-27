eb9246c52260f21dd0d0bbde72fa4d3627c72a65
ste14288 <ste14288@spengergasse.at>
2013-09-27 12:29:36 +0200

    Update A.java

--- 4chif/src/A.java
+++ 4chif/src/A.java
@@ -2,7 +2,7 @@ public class A {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hallo git 222")
+		System.out.println("hallo git 2222")
 
 	}
 
